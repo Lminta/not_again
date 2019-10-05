3faa49e8dd68bbfbebf2eb1244405829c0aa0a24
commanotadot <49369209+commanotadot@users.noreply.github.com>
2019-10-05 18:35:53 +0300

    done for today

--- Assets/mv_runner_s.cs
+++ Assets/mv_runner_s.cs
@@ -5,28 +5,47 @@ using UnityEngine;
 public class mv_runner_s : MonoBehaviour
 {
     private Rigidbody2D rb2D;
+    Vector2 shake = Vector2.zero;
+
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
+        InvokeRepeating("Shaking", 3, 1);
     }
 
     void Update()
     {
         if (Input.GetKey("up"))
         {
-            rb2D.AddForce(Vector2.up);
+            rb2D.AddForce(Vector2.up * 1.5f);
         }
         if (Input.GetKey("down"))
         {
-            rb2D.AddForce(Vector2.down);
+            rb2D.AddForce(Vector2.down * 1.5f);
         }
         if (Input.GetKey("right"))
         {
-            rb2D.AddForce(Vector2.right);
+            rb2D.AddForce(Vector2.right * 1.5f);
         }
         if (Input.GetKey("left"))
         {
-            rb2D.AddForce(Vector2.left);
+            rb2D.AddForce(Vector2.left * 1.5f);
         }
+        rb2D.AddForce(shake);
+    }
+
+    void Shaking()
+    {
+        //shake = new Vector2(0.5f, 0.5f);
+        shake = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f) * Time.time / 5.0f);
+        if (shake.x > 1.0f || shake.x < -1.0f)
+        {
+            shake.x = shake.x < 0 ? -1.0f : 1.0f;
+        }
+        if (shake.y > 1.0f || shake.y < -1.0f)
+        {
+            shake.y = shake.y < 0 ? -1.0f : 1.0f;
+        }
+        Debug.Log(shake);
     }
 }
