e5e5bc5d767ad17195e5948252d2e501fd93a2bf
lminta <lminta@student.21-school.ru>
2019-10-05 17:31:22 +0300

    huhu

---
+++ Assets/Spawner.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> stationsPrefabs = new List<GameObject>();
+    public List<GameObject> debrisPrefabs = new List<GameObject>(); 
+    public int amountEnemies = 20;
+
+    public float EPSILON { get; private set; }
+
+    private void Start()
+    {
+        SpawnSites();
+        SpawnStations();
+    }
+
+    public void SpawnSites()
+    {
+        for (int i = 0; i < amountEnemies; i++)
+        {
+            GameObject prefab = debrisPrefabs[Random.Range(0, debrisPrefabs.Count)];
+            var ranVec = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 1);
+            if (System.Math.Abs(ranVec.x) > EPSILON && System.Math.Abs(ranVec.y) > EPSILON)
+            {
+                prefab.transform.position = ranVec;
+                Instantiate(prefab, ranVec, prefab.transform.rotation);
+            }
+            else
+                i--;
+        }
+    }
+
+    public void SpawnStations()
+    {
+        for (int i = 0; i < stationsPrefabs.Count; i++)
+        {
+
+            GameObject prefab = stationsPrefabs[Random.Range(0, stationsPrefabs.Count)];
+            var ranVec = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 1);
+            if (System.Math.Abs(ranVec.x) > EPSILON && System.Math.Abs(ranVec.y) > EPSILON)
+            {
+                prefab.transform.position = ranVec;
+                Instantiate(prefab, ranVec, prefab.transform.rotation);
+            }
+            else
+                i--;
+        }
+    }
+}
