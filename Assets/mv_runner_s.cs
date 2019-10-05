8ecbc389faec271a6c580e97915c83697721d897
commanotadot <49369209+commanotadot@users.noreply.github.com>
2019-10-05 17:30:01 +0300

    backup

---
+++ Assets/mv_runner_s.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mv_runner_s : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            rb2D.AddForce(Vector2.up);
+        }
+        if (Input.GetKey("down"))
+        {
+            rb2D.AddForce(Vector2.down);
+        }
+        if (Input.GetKey("right"))
+        {
+            rb2D.AddForce(Vector2.right);
+        }
+        if (Input.GetKey("left"))
+        {
+            rb2D.AddForce(Vector2.left);
+        }
+    }
+}
