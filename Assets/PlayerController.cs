65b10644581f6fceda53bf97613db978fcbf025c
lminta <lminta@student.21-school.ru>
2019-10-05 18:35:22 +0300

    huhuhu

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector3 movement = new Vector3(transform.position.x + moveHorizontal * speed, 
+        transform.position.y + moveVertical * speed, 
+        transform.position.z);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        transform.position = movement;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag("MapBorder"))
+            SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
+    }
+}
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -8,8 +8,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     public List<GameObject> stationsPrefabs = new List<GameObject>();
-    public List<GameObject> debrisPrefabs = new List<GameObject>(); 
-    public int amountEnemies = 20;
+    public List<GameObject> debrisPrefabs = new List<GameObject>();
+    public GameObject Player;
+    public int amountSites = 20;
 
     public float EPSILON { get; private set; }
 
@@ -17,11 +18,12 @@ public class Spawner : MonoBehaviour
     {
         SpawnSites();
         SpawnStations();
+        SpawnPlayer();
     }
 
     public void SpawnSites()
     {
-        for (int i = 0; i < amountEnemies; i++)
+        for (int i = 0; i < amountSites; i++)
         {
             GameObject prefab = debrisPrefabs[Random.Range(0, debrisPrefabs.Count)];
             var ranVec = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 1);
@@ -38,17 +40,17 @@ public class Spawner : MonoBehaviour
     public void SpawnStations()
     {
         for (int i = 0; i < stationsPrefabs.Count; i++)
-        {
-
-            GameObject prefab = stationsPrefabs[Random.Range(0, stationsPrefabs.Count)];
+        { 
+        GameObject prefab = stationsPrefabs[i];
             var ranVec = new Vector3(Random.Range(-9, 9), Random.Range(-5, 5), 1);
-            if (System.Math.Abs(ranVec.x) > EPSILON && System.Math.Abs(ranVec.y) > EPSILON)
-            {
-                prefab.transform.position = ranVec;
-                Instantiate(prefab, ranVec, prefab.transform.rotation);
-            }
-            else
-                i--;
+            prefab.transform.position = ranVec;
+            Instantiate(prefab, ranVec, prefab.transform.rotation);
         }
     }
-}
+
+    public void SpawnPlayer()
+    {
+        var ranVec = Vector3.zero;
+        Instantiate(Player, ranVec, Player.transform.rotation);
+    }
+ }
