cb3cdce9e0e17552dbdd0f4054af65db2e331d8f
olesgedz <olesgedz@rambler.ru>
2019-10-07 07:21:32 +0300

    Panels

---
+++ Assets/Scripts/UI/TestModalWindow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class TestModalWindow : MonoBehaviour
+{
+    private ModalPanel modalPanel;
+
+    private UnityAction rightAction;
+
+    private UnityAction leftAction;
+
+    private void Awake()
+    {
+        modalPanel = ModalPanel.Instance();
+        rightAction = new UnityAction(testRight);
+        leftAction = new UnityAction(testLeft);
+
+    }
+    public void TestModal()
+    {
+        modalPanel.Choice("Hello, you have been attackted by wolfs", leftAction, rightAction);
+    }
+
+    void testRight()
+    {
+        Debug.Log("right");
+    }
+
+    void testLeft()
+    {
+        Debug.Log("butWhy?");
+    }
+}
